103b60d071c134e6dbef0aed92bae8a1f68826ea
thehsadowte <103116337+thehsadowte@users.noreply.github.com>
2022-11-16 13:29:22 +0000

    Deploying to gh-pages from @ thehsadowte/goit-js-hw-09@de268670136b3095149ddf03fb56f88c16144340 🚀

---
+++ 01-color-switcher.21ce6af8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d;e.addEventListener("click",(function(){d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.21ce6af8.js.map
